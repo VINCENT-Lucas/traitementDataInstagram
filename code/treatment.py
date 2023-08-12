58ac4264f764060434e2af88df924eae156031c0
Lucas <100359369+Lucasssstor@users.noreply.github.com>
2023-08-12 21:50:05 +0200

    Update treatment.py

--- code/treatment.py
+++ code/treatment.py
@@ -1,6 +1,7 @@
 from code.utilitaries import *
 from code.html import *
 
+import datetime, time
 
 ''' On met à jour les compteurs de mots'''
 def updateWordCounter(msg, username, wordCounterDic):
@@ -9,37 +10,39 @@ def updateWordCounter(msg, username, wordCounterDic):
         updateDict(wordCounterDic, content.split())
          
 ''' On met à jour le compteur de messages reçus à un certain horaire '''
-def updateHoursCounter(msg, username, timeTableDic):
+def updateHoursCounter(msg, timeTableDic):
   hour = timeStampToHour(msg["timestamp_ms"])[:3]
   updateDict(timeTableDic, [hour])
 
-   
+
 ''' Traite l'ensemble des données (fichiers json contenant les discussions) '''
-def treatment(filesList, username, wordCounterDic, sentMsgTimeTableDic, receivedMsgTimeTableDic):
+def treatment(filesList, username, wordCounterDic, sentMsgTimeTableDic, receivedMsgTimeTableDic, amountOfParticipantsDic):
   for file_path in filesList:
     with open(file_path, "r", encoding="utf-8") as file:
-      json_data = json.load(file)
-      conversationName = json_data["title"].encode('latin1').decode('utf-8')
-      messages = json_data["messages"]
+      jsonData = json.load(file)
+      conversationName = jsonData["title"].encode('latin1').decode('utf-8')
+      messages = jsonData["messages"]
+
+      amountOfParticipantsDic[conversationName] = len(jsonData["participants"])
 
       for msg in messages:
         updateWordCounter(msg, username, wordCounterDic)
-        updateHoursCounter(msg, username, sentMsgTimeTableDic) if msg["sender_name"] == username else updateHoursCounter(msg, username, receivedMsgTimeTableDic)
+        updateHoursCounter(msg, sentMsgTimeTableDic) if msg["sender_name"] == username else updateHoursCounter(msg, receivedMsgTimeTableDic)
         
    
 ''' Traitement de tous les fichiers associés à un utilisateur (par exemple messages1, messages2 et messages3 pour User1).
 On prend en entrée la liste des json associés à un utilisateur unique.
 '''
-def ConversationfilesProcess(jsonList, messageAmountDic, wordCounterDic, conversationAgeDic, dirName, userInfoDic, username, dirPaths):
+def ConversationTreatment(jsonList, conversationSizeDic, wordCounterDic, conversationAgeDic, dirName, userInfoDic, username, dirPaths, conversationMaxStreakDic, amountOfMessPerDayDic):
     orderedDiscussions = order_files(jsonList)
     convData = {"amountOfSentMsg":0, "amountOfReceivedMsg":0, "amountOfSentAns":0, "amountOfReceivedAns":0, "lastWhoTalked":None, 
     "timeBeforeAnswering":0, "timeBeforeGettingAnswered":0, "biggestStreak": 0, "sizeOfSentMessages": 0, "sizeOfReceivedMessages": 0,
     "lastSentMessageTime": 0, "lastReceivedMessageTime":0, "conversationName": ""}
     calendarList, daysList = [], []
-    conversationWordCounter = {}
+    conversationWordCounter, hoursDic = {}, {}
 
     with open(os.path.join(dirPaths['Conversations'], dirName+'.html'), 'w', encoding="utf-8") as ConversationFile:
-        ConversationFile.write(getHTMLConvBeginning(convData["conversationName"]))
+        ConversationFile.write(getHTMLConvHeader(convData["conversationName"]) + '\n<body>\n<div class="container">')
         
         for file_path in orderedDiscussions:
             with open(file_path, "r", encoding="utf-8") as file:
@@ -50,27 +53,33 @@ def ConversationfilesProcess(jsonList, messageAmountDic, wordCounterDic, convers
                 messages = json_data["messages"]
 
                 updateUserInfo(userInfoDic, dirName, convData["conversationName"])
-                updateMessageAmount(messageAmountDic, convData["conversationName"], messages)
-                
-                messagesTreatment(messages, ConversationFile, convData, calendarList, username, conversationAgeDic, conversationWordCounter)
+
+                messagesTreatment(messages, ConversationFile, convData, calendarList, username, conversationAgeDic, conversationWordCounter, hoursDic, amountOfMessPerDayDic)
         ConversationFile.write("</div>\n</body>\n</html>")
+    
+    updateMessageAmount(conversationSizeDic, convData)
     calendarTreatment(calendarList, daysList)
     writeCalendar(daysList, os.path.join(dirPaths['Calendar'], dirName+'.html'), convData, nextDay, dirPaths, dirName)
 
-    writeMostUsedWords(conversationWordCounter, wordCounterDic, os.path.join(dirPaths['Words'], dirName+'.html'), convData["conversationName"], dirName, dirPaths)
+    conversationMaxStreakDic[convData["conversationName"]] = convData["biggestStreak"]
 
-    writeDataConversation(dirName, convData, calendarList, dirPaths, username)
+    writeMostUsedWords(conversationWordCounter, wordCounterDic, os.path.join(dirPaths['Words'], dirName+'.html'), convData["conversationName"], dirName, dirPaths, hoursDic)
+
+    writeDataConversation(dirName, convData, calendarList, dirPaths, username, hoursDic)
+      
 
 ''' On effectue tous les traitements sur tous les messages.
 C'EST ICI QU'ON ECRIT LES CONVERSATIONS.'''
-def messagesTreatment(messages, ConvFile, convData, calendarList, username, conversationAge, conversationWordCounter):
+def messagesTreatment(messages, ConvFile, convData, calendarList, username, conversationAge, conversationWordCounter, hoursDic, amountOfMessPerDayDic):
     conversationName = convData["conversationName"]
     min = 1000*time.time()
     ancientDate = None
     for msg in messages[::-1]:
-      messageDate = convert_timestamp(datetime.datetime.fromtimestamp(msg["timestamp_ms"]/1000))
+      messageDate = convertTimestamp(datetime.datetime.fromtimestamp(msg["timestamp_ms"]/1000))
       txt = ''
       
+      updateHoursCounter(msg, hoursDic)
+
       # On récupère le plus ancien message du fichier
       if msg["timestamp_ms"] < min:
         min = msg["timestamp_ms"]
@@ -79,7 +88,8 @@ def messagesTreatment(messages, ConvFile, convData, calendarList, username, conv
       if "content" in msg:
         calendarList.append(msg["timestamp_ms"])
         if msg["sender_name"] == username:
-          updateDict(conversationWordCounter, msg["content"].split(" "))
+          updateDict(amountOfMessPerDayDic, [messageDate[:10]])
+          updateDict(conversationWordCounter, msg["content"].encode('latin1').decode('utf-8').split(" "))
           convData["amountOfSentMsg"] += 1
           convData["sizeOfSentMessages"] += len(msg["content"])
           if convData["lastWhoTalked"] != msg["sender_name"] and convData["lastWhoTalked"] != None:
@@ -111,7 +121,6 @@ def messagesTreatment(messages, ConvFile, convData, calendarList, username, conv
     if conversationName not in conversationAge or conversationAge[conversationName] > datetime.datetime.fromtimestamp(min/1000):
         conversationAge[conversationName] = datetime.datetime.fromtimestamp(min/1000)
 
-    
 
 ''' Traitement de la liste, on extrait d'une liste de timecodes une liste de jours uniques '''
 def calendarTreatment(calendarList, daysList):
@@ -120,3 +129,9 @@ def calendarTreatment(calendarList, daysList):
     date = datetime.datetime.fromtimestamp(timecode/1000).strftime("%d/%m/%Y")
     if date not in daysList:
       daysList.append(date)
+
+''' Calcule les scores de complicité de toutes les conversations '''
+def computeComplicityScores(complicityScore, amountOfParticipantsDic, conversationAgeDic, conversationSizeDic, conversationMaxStreakDic):
+   for key in conversationSizeDic:
+      if amountOfParticipantsDic[key] == 2:
+        complicityScore[key] = dateTimeToDays(conversationAgeDic[key]) * int((conversationSizeDic[key][0] * conversationSizeDic[key][1])**(1/4)) * conversationMaxStreakDic[key]
